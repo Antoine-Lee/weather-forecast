2ccb400ba41364615266d382758dd71e159c0798
Antoine <62200697+antoines-game-dev@users.noreply.github.com>
2020-09-20 17:24:00 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
-// const cityInput = document.getElementById ("cityInput")
-// const submitCityButton = document.getElementById("submitCity")
-// const cityInfo = document.getElementById ("cityInfo")
-
 const cityInput = document.querySelector ("#cityInput")
 const submitCityButton = document.querySelector("#submitCity")
 const cityName = document.querySelector ("#cityName")
 
-let weatherInfo = document.querySelector("#weatherInfo")
-let dates = document.querySelectorAll (".date")
-let statusIcons = document.querySelectorAll (".statusIcon")
-let status = document.querySelectorAll (".status")
-let temperature = document.querySelectorAll (".temperature")
-let minTemperature = document.querySelectorAll (".minTemperature")
-let maxTemperature = document.querySelectorAll (".maxTemperature")
+let weatherInfo = document.querySelector ("#weatherInfo")
+
+let dates = null
+let statusIcons = null
+let status = null
+let temperature = null
+let minTemperature = null
+let maxTemperature = null
+let windSpeeds = null
+let windDirections = null
 
 let firstSearch = false
 
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
 let UpdateCityDropdown = async () => 
 {
-    let response = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${cityInput.value}`)
+    let response = await fetch(`https://www.metaweather.com/api/location/search/?query=${cityInput.value}`)
     let json = await response.json()
 
     cityDropdown.innerHTML = '<option>Select</option>\n'
@@ -30,27 +31,25 @@ let UpdateCityDropdown = async () =>
 
 let CheckWeatherForecast = async () => 
 {
-    if (cityDropdown.value == "None")
+    if (cityDropdown.value == "Select")
     {
         alert ("Please select a city")
         return
     }
 
-    let response = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search/?query=${cityDropdown.value}`)
+    let response = await fetch(`https://www.metaweather.com/api/location/search/?query=${cityDropdown.value}`)
     let json = await response.json()
-
-    console.log (json[0].woeid)
 
     GetCityData(woeid = json[0].woeid)
 }
 
 let GetCityData = async (woeid) => 
 {
-    let response = await fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${woeid}/`)
+    let response = await fetch(`https://www.metaweather.com/api/location/${woeid}/`)
     let json = await response.json()
     
     if (json.title != json.parent.title)
-        cityName.innerHTML = `${json.title} ${json.location_type} in ${json.parent.title}`
+        cityName.innerHTML = `${json.title} : ${json.location_type} in ${json.parent.title}`
     else
         cityName.innerHTML = `${json.title}`
 
@@ -72,16 +71,19 @@ let GetCityData = async (woeid) =>
                                         <h3 class="temperature"></h3>
                                         <h3 class="minTemperature"></h3>
                                         <h3 class="maxTemperature"></h3>
+                                        <span class="windDirection"></span>
+                                        <h3 class="windSpeed"></h3>
                                     </div>`
         }
 
-        weatherInfo = document.querySelector("#weatherInfo")
         dates = document.querySelectorAll(".date")
         statusIcons = document.querySelectorAll(".statusIcon")
         status = document.querySelectorAll(".status")
         temperature = document.querySelectorAll(".temperature")
         minTemperature = document.querySelectorAll(".minTemperature")
         maxTemperature = document.querySelectorAll(".maxTemperature")
+        windSpeeds = document.querySelectorAll(".windSpeed")
+        windDirections = document.querySelectorAll(".windDirection")
 
         firstSearch = true; 
     }
@@ -91,62 +93,24 @@ let GetCityData = async (woeid) =>
         if (index <= 4)
         {
             if (index >= 2)
-                dates[index].innerHTML = `${forecast.applicable_date}`
-            
+            {
+                unformattedDate = forecast.applicable_date
+                formattedDateData = unformattedDate.split ("-")
+                formattedDateData[1] = parseInt(formattedDateData[1], 10)
+                date = new Date(formattedDateData[0], formattedDateData[1] - 1, formattedDateData[2], 0, 0, 0, 0)
+                dates[index].innerHTML = weekdays[date.getDay()];
+            }
+
             statusIcons[index].src = `https://www.metaweather.com/static/img/weather/${forecast.weather_state_abbr}.svg`
             status[index].innerHTML = `${forecast.weather_state_name}`
             temperature[index].innerHTML = `${Math.round(forecast.the_temp * 10) / 10}`
             minTemperature [index].innerHTML = `Min: ${Math.round(forecast.min_temp * 10) / 10}`
             maxTemperature [index].innerHTML = `Max: ${Math.round(forecast.max_temp * 10) / 10}`
+            windDirections[index].style.transform = `rotate(${forecast.wind_direction}deg)`
+            windSpeeds[index].innerHTML = `${Math.round(forecast.wind_speed * 10) / 10} mph`
         }
     })
 }
 
 cityInput.addEventListener("change", UpdateCityDropdown);
 submitCityButton.addEventListener("click", CheckWeatherForecast);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let value = document.getElementById ("value")
-// let exchangeFrom = document.getElementById ("exchangeFrom")
-// let exchangeTo = document.getElementById ("exchangeTo")
-// let displaySelected = document.getElementById ("displaySelection")
-// let displayAll = document.getElementById ("displayAll")
-
-// let GetCurrancyData = async (currencyFrom, currency, currencyTo) => {
-//     let response = await fetch(`https://api.exchangerate-api.com/v4/latest/${currencyFrom}`)
-//     let json = await response.json()
-    
-//     displaySelected.innerHTML = ''
-//     displaySelected.innerHTML = `<h1>${currencyTo}: ${Math.floor(json.rates[currencyTo] * currency * 100) / 100}</h1>`
-    
-//     let ratesArray = Object.entries (json.rates) // Convert json.rates dictionary into array
-    
-//     displayAll.innerHTML = ''
-//     ratesArray.map((object) => {
-//         if (object[0] != currencyFrom && object[0] != currencyTo)
-//         displayAll.innerHTML += `<p>${object[0]} : ${Math.floor(object[1] * currency * 100) / 100}</p>\n`
-//     })
-// }
-
-// let UpdateData = () => {
-//     currency = value.value
-//     currencyFrom = exchangeFrom.options[exchangeFrom.selectedIndex].value
-//     currencyTo = exchangeTo.options[exchangeTo.selectedIndex].value
-//     GetCurrancyData(currencyFrom, currency, currencyTo)
-// }
-
-// exchangeFrom.addEventListener("change", UpdateData)
-// value.addEventListener("change", UpdateData)
-// exchangeTo.addEventListener("change", UpdateData)
